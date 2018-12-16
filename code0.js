4382b645136fa049d75206cb8e739c2b30210c3b
triloute <45915223+triloute@users.noreply.github.com>
2018-12-16 13:09:00 +0100

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,380 @@
+gdjs.landscapeCode = {};
+gdjs.landscapeCode.GDmichonneObjects1= [];
+gdjs.landscapeCode.GDmichonneObjects2= [];
+gdjs.landscapeCode.GDmichonneObjects3= [];
+gdjs.landscapeCode.GDZGObjects1= [];
+gdjs.landscapeCode.GDZGObjects2= [];
+gdjs.landscapeCode.GDZGObjects3= [];
+gdjs.landscapeCode.GDZDObjects1= [];
+gdjs.landscapeCode.GDZDObjects2= [];
+gdjs.landscapeCode.GDZDObjects3= [];
+gdjs.landscapeCode.GDBGObjects1= [];
+gdjs.landscapeCode.GDBGObjects2= [];
+gdjs.landscapeCode.GDBGObjects3= [];
+gdjs.landscapeCode.GDBDObjects1= [];
+gdjs.landscapeCode.GDBDObjects2= [];
+gdjs.landscapeCode.GDBDObjects3= [];
+gdjs.landscapeCode.GDtimebar2Objects1= [];
+gdjs.landscapeCode.GDtimebar2Objects2= [];
+gdjs.landscapeCode.GDtimebar2Objects3= [];
+gdjs.landscapeCode.GDtempsObjects1= [];
+gdjs.landscapeCode.GDtempsObjects2= [];
+gdjs.landscapeCode.GDtempsObjects3= [];
+gdjs.landscapeCode.GDGame_95OverObjects1= [];
+gdjs.landscapeCode.GDGame_95OverObjects2= [];
+gdjs.landscapeCode.GDGame_95OverObjects3= [];
+gdjs.landscapeCode.GDbulleObjects1= [];
+gdjs.landscapeCode.GDbulleObjects2= [];
+gdjs.landscapeCode.GDbulleObjects3= [];
+
+gdjs.landscapeCode.conditionTrue_0 = {val:false};
+gdjs.landscapeCode.condition0IsTrue_0 = {val:false};
+gdjs.landscapeCode.condition1IsTrue_0 = {val:false};
+gdjs.landscapeCode.condition2IsTrue_0 = {val:false};
+gdjs.landscapeCode.condition3IsTrue_0 = {val:false};
+gdjs.landscapeCode.conditionTrue_1 = {val:false};
+gdjs.landscapeCode.condition0IsTrue_1 = {val:false};
+gdjs.landscapeCode.condition1IsTrue_1 = {val:false};
+gdjs.landscapeCode.condition2IsTrue_1 = {val:false};
+gdjs.landscapeCode.condition3IsTrue_1 = {val:false};
+
+
+gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects2Objects = Hashtable.newFrom({"ZG": gdjs.landscapeCode.GDZGObjects2});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects1Objects = Hashtable.newFrom({"ZD": gdjs.landscapeCode.GDZDObjects1});gdjs.landscapeCode.eventsList0xf025148 = function(runtimeScene, context) {
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(1)) == 0;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDZGObjects2.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects2Objects, 182, 190, "");
+}}
+
+}
+
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(1)) == 1;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDZDObjects1.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects1Objects, 852, 190, "");
+}}
+
+}
+
+
+}; //End of gdjs.landscapeCode.eventsList0xf025148
+gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDGame_9595OverObjects1Objects = Hashtable.newFrom({"Game_Over": gdjs.landscapeCode.GDGame_95OverObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects = Hashtable.newFrom({"BG": gdjs.landscapeCode.GDBGObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects = Hashtable.newFrom({"ZG": gdjs.landscapeCode.GDZGObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects = Hashtable.newFrom({"BG": gdjs.landscapeCode.GDBGObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects2Objects = Hashtable.newFrom({"ZD": gdjs.landscapeCode.GDZDObjects2});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects = Hashtable.newFrom({"ZG": gdjs.landscapeCode.GDZGObjects1});gdjs.landscapeCode.eventsList0xf025f88 = function(runtimeScene, context) {
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(1)) == 1;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDZDObjects2.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects2Objects, 852, 190, "");
+}}
+
+}
+
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(1)) == 0;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+/* Reuse gdjs.landscapeCode.GDZGObjects1 */
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects, 182, 190, "");
+}}
+
+}
+
+
+}; //End of gdjs.landscapeCode.eventsList0xf025f88
+gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects = Hashtable.newFrom({"BD": gdjs.landscapeCode.GDBDObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects1Objects = Hashtable.newFrom({"ZD": gdjs.landscapeCode.GDZDObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects = Hashtable.newFrom({"BD": gdjs.landscapeCode.GDBDObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects2Objects = Hashtable.newFrom({"ZD": gdjs.landscapeCode.GDZDObjects2});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects = Hashtable.newFrom({"ZG": gdjs.landscapeCode.GDZGObjects1});gdjs.landscapeCode.eventsList0xf026408 = function(runtimeScene, context) {
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(1)) == 1;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDZDObjects2.createFrom(gdjs.landscapeCode.GDZDObjects1);
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects2Objects, 852, 190, "");
+}}
+
+}
+
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(1)) == 0;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDZGObjects1.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects, 182, 190, "");
+}}
+
+}
+
+
+}; //End of gdjs.landscapeCode.eventsList0xf026408
+gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects = Hashtable.newFrom({"BD": gdjs.landscapeCode.GDBDObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects = Hashtable.newFrom({"ZG": gdjs.landscapeCode.GDZGObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects = Hashtable.newFrom({"BG": gdjs.landscapeCode.GDBGObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDGame_9595OverObjects1Objects = Hashtable.newFrom({"Game_Over": gdjs.landscapeCode.GDGame_95OverObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects = Hashtable.newFrom({"BG": gdjs.landscapeCode.GDBGObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects1Objects = Hashtable.newFrom({"ZD": gdjs.landscapeCode.GDZDObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects = Hashtable.newFrom({"BD": gdjs.landscapeCode.GDBDObjects1});gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDGame_9595OverObjects1Objects = Hashtable.newFrom({"Game_Over": gdjs.landscapeCode.GDGame_95OverObjects1});gdjs.landscapeCode.eventsList0x70e0f0 = function(runtimeScene, context) {
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+{runtimeScene.getVariables().getFromIndex(1).setNumber(gdjs.random(1));
+}
+{ //Subevents
+gdjs.landscapeCode.eventsList0xf025148(runtimeScene, context);} //End of subevents
+}
+
+}
+
+
+{
+
+
+{
+gdjs.landscapeCode.GDtimebar2Objects1.createFrom(runtimeScene.getObjects("timebar2"));
+{runtimeScene.getVariables().getFromIndex(3).sub(10*gdjs.evtTools.runtimeScene.getElapsedTimeInSeconds(runtimeScene));
+}{for(var i = 0, len = gdjs.landscapeCode.GDtimebar2Objects1.length ;i < len;++i) {
+    gdjs.landscapeCode.GDtimebar2Objects1[i].setScaleX(gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(3))/30);
+}
+}}
+
+}
+
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(3)) < 0;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDGame_95OverObjects1.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDGame_9595OverObjects1Objects, 350, 330, "gui");
+}{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "GO", false);
+}}
+
+}
+
+
+{
+
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_0.val = gdjs.evtTools.common.getVariableNumber(runtimeScene.getVariables().getFromIndex(3)) > 200;
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+{runtimeScene.getVariables().getFromIndex(3).setNumber(200);
+}}
+
+}
+
+
+{
+
+gdjs.landscapeCode.GDBGObjects1.createFrom(runtimeScene.getObjects("BG"));
+gdjs.landscapeCode.GDZGObjects1.createFrom(runtimeScene.getObjects("ZG"));
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+{gdjs.landscapeCode.conditionTrue_1 = gdjs.landscapeCode.condition0IsTrue_0;
+gdjs.landscapeCode.condition0IsTrue_1.val = false;
+gdjs.landscapeCode.condition1IsTrue_1.val = false;
+gdjs.landscapeCode.condition2IsTrue_1.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_1.val = gdjs.evtTools.input.isMouseButtonReleased(runtimeScene, "Left");
+}if ( gdjs.landscapeCode.condition0IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition1IsTrue_1.val = gdjs.evtTools.input.cursorOnObject(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects, runtimeScene, true, false);
+}if ( gdjs.landscapeCode.condition1IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition2IsTrue_1.val = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects, false, runtimeScene);
+}}
+}
+gdjs.landscapeCode.conditionTrue_1.val = true && gdjs.landscapeCode.condition0IsTrue_1.val && gdjs.landscapeCode.condition1IsTrue_1.val && gdjs.landscapeCode.condition2IsTrue_1.val;
+}
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+/* Reuse gdjs.landscapeCode.GDZGObjects1 */
+{for(var i = 0, len = gdjs.landscapeCode.GDZGObjects1.length ;i < len;++i) {
+    gdjs.landscapeCode.GDZGObjects1[i].deleteFromScene(runtimeScene);
+}
+}{runtimeScene.getVariables().getFromIndex(1).setNumber(gdjs.random(1));
+}{runtimeScene.getVariables().getFromIndex(3).add(2);
+}
+{ //Subevents
+gdjs.landscapeCode.eventsList0xf025f88(runtimeScene, context);} //End of subevents
+}
+
+}
+
+
+{
+
+gdjs.landscapeCode.GDBDObjects1.createFrom(runtimeScene.getObjects("BD"));
+gdjs.landscapeCode.GDZDObjects1.createFrom(runtimeScene.getObjects("ZD"));
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+{gdjs.landscapeCode.conditionTrue_1 = gdjs.landscapeCode.condition0IsTrue_0;
+gdjs.landscapeCode.condition0IsTrue_1.val = false;
+gdjs.landscapeCode.condition1IsTrue_1.val = false;
+gdjs.landscapeCode.condition2IsTrue_1.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_1.val = gdjs.evtTools.input.isMouseButtonReleased(runtimeScene, "Left");
+}if ( gdjs.landscapeCode.condition0IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition1IsTrue_1.val = gdjs.evtTools.input.cursorOnObject(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects, runtimeScene, true, false);
+}if ( gdjs.landscapeCode.condition1IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition2IsTrue_1.val = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects1Objects, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects, false, runtimeScene);
+}}
+}
+gdjs.landscapeCode.conditionTrue_1.val = true && gdjs.landscapeCode.condition0IsTrue_1.val && gdjs.landscapeCode.condition1IsTrue_1.val && gdjs.landscapeCode.condition2IsTrue_1.val;
+}
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+/* Reuse gdjs.landscapeCode.GDZDObjects1 */
+{for(var i = 0, len = gdjs.landscapeCode.GDZDObjects1.length ;i < len;++i) {
+    gdjs.landscapeCode.GDZDObjects1[i].deleteFromScene(runtimeScene);
+}
+}{runtimeScene.getVariables().getFromIndex(1).setNumber(gdjs.random(1));
+}{runtimeScene.getVariables().getFromIndex(3).add(2);
+}
+{ //Subevents
+gdjs.landscapeCode.eventsList0xf026408(runtimeScene, context);} //End of subevents
+}
+
+}
+
+
+{
+
+gdjs.landscapeCode.GDBDObjects1.createFrom(runtimeScene.getObjects("BD"));
+gdjs.landscapeCode.GDBGObjects1.createFrom(runtimeScene.getObjects("BG"));
+gdjs.landscapeCode.GDZGObjects1.createFrom(runtimeScene.getObjects("ZG"));
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+{gdjs.landscapeCode.conditionTrue_1 = gdjs.landscapeCode.condition0IsTrue_0;
+gdjs.landscapeCode.condition0IsTrue_1.val = false;
+gdjs.landscapeCode.condition1IsTrue_1.val = false;
+gdjs.landscapeCode.condition2IsTrue_1.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_1.val = gdjs.evtTools.input.isMouseButtonPressed(runtimeScene, "Left");
+}if ( gdjs.landscapeCode.condition0IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition1IsTrue_1.val = gdjs.evtTools.input.cursorOnObject(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects, runtimeScene, true, false);
+}if ( gdjs.landscapeCode.condition1IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition2IsTrue_1.val = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZGObjects1Objects, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects, false, runtimeScene);
+}}
+}
+gdjs.landscapeCode.conditionTrue_1.val = true && gdjs.landscapeCode.condition0IsTrue_1.val && gdjs.landscapeCode.condition1IsTrue_1.val && gdjs.landscapeCode.condition2IsTrue_1.val;
+}
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDGame_95OverObjects1.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDGame_9595OverObjects1Objects, 350, 330, "gui");
+}{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "GO", false);
+}}
+
+}
+
+
+{
+
+gdjs.landscapeCode.GDBDObjects1.createFrom(runtimeScene.getObjects("BD"));
+gdjs.landscapeCode.GDBGObjects1.createFrom(runtimeScene.getObjects("BG"));
+gdjs.landscapeCode.GDZDObjects1.createFrom(runtimeScene.getObjects("ZD"));
+
+gdjs.landscapeCode.condition0IsTrue_0.val = false;
+{
+{gdjs.landscapeCode.conditionTrue_1 = gdjs.landscapeCode.condition0IsTrue_0;
+gdjs.landscapeCode.condition0IsTrue_1.val = false;
+gdjs.landscapeCode.condition1IsTrue_1.val = false;
+gdjs.landscapeCode.condition2IsTrue_1.val = false;
+{
+gdjs.landscapeCode.condition0IsTrue_1.val = gdjs.evtTools.input.isMouseButtonPressed(runtimeScene, "Left");
+}if ( gdjs.landscapeCode.condition0IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition1IsTrue_1.val = gdjs.evtTools.input.cursorOnObject(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBGObjects1Objects, runtimeScene, true, false);
+}if ( gdjs.landscapeCode.condition1IsTrue_1.val ) {
+{
+gdjs.landscapeCode.condition2IsTrue_1.val = gdjs.evtTools.object.hitBoxesCollisionTest(gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDZDObjects1Objects, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDBDObjects1Objects, false, runtimeScene);
+}}
+}
+gdjs.landscapeCode.conditionTrue_1.val = true && gdjs.landscapeCode.condition0IsTrue_1.val && gdjs.landscapeCode.condition1IsTrue_1.val && gdjs.landscapeCode.condition2IsTrue_1.val;
+}
+}if (gdjs.landscapeCode.condition0IsTrue_0.val) {
+gdjs.landscapeCode.GDGame_95OverObjects1.length = 0;
+
+{gdjs.evtTools.object.createObjectOnScene(runtimeScene, gdjs.landscapeCode.mapOfGDgdjs_46landscapeCode_46GDGame_9595OverObjects1Objects, 350, 330, "gui");
+}{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "GO", false);
+}}
+
+}
+
+
+}; //End of gdjs.landscapeCode.eventsList0x70e0f0
+
+
+gdjs.landscapeCode.func = function(runtimeScene, context) {
+context.startNewFrame();
+gdjs.landscapeCode.GDmichonneObjects1.length = 0;
+gdjs.landscapeCode.GDmichonneObjects2.length = 0;
+gdjs.landscapeCode.GDmichonneObjects3.length = 0;
+gdjs.landscapeCode.GDZGObjects1.length = 0;
+gdjs.landscapeCode.GDZGObjects2.length = 0;
+gdjs.landscapeCode.GDZGObjects3.length = 0;
+gdjs.landscapeCode.GDZDObjects1.length = 0;
+gdjs.landscapeCode.GDZDObjects2.length = 0;
+gdjs.landscapeCode.GDZDObjects3.length = 0;
+gdjs.landscapeCode.GDBGObjects1.length = 0;
+gdjs.landscapeCode.GDBGObjects2.length = 0;
+gdjs.landscapeCode.GDBGObjects3.length = 0;
+gdjs.landscapeCode.GDBDObjects1.length = 0;
+gdjs.landscapeCode.GDBDObjects2.length = 0;
+gdjs.landscapeCode.GDBDObjects3.length = 0;
+gdjs.landscapeCode.GDtimebar2Objects1.length = 0;
+gdjs.landscapeCode.GDtimebar2Objects2.length = 0;
+gdjs.landscapeCode.GDtimebar2Objects3.length = 0;
+gdjs.landscapeCode.GDtempsObjects1.length = 0;
+gdjs.landscapeCode.GDtempsObjects2.length = 0;
+gdjs.landscapeCode.GDtempsObjects3.length = 0;
+gdjs.landscapeCode.GDGame_95OverObjects1.length = 0;
+gdjs.landscapeCode.GDGame_95OverObjects2.length = 0;
+gdjs.landscapeCode.GDGame_95OverObjects3.length = 0;
+gdjs.landscapeCode.GDbulleObjects1.length = 0;
+gdjs.landscapeCode.GDbulleObjects2.length = 0;
+gdjs.landscapeCode.GDbulleObjects3.length = 0;
+
+gdjs.landscapeCode.eventsList0x70e0f0(runtimeScene, context);return;
+}
+gdjs['landscapeCode']= gdjs.landscapeCode;
---
+++ code1.js
@@ -0,0 +1,17 @@
+gdjs.GOCode = {};
+
+gdjs.GOCode.conditionTrue_0 = {val:false};
+gdjs.GOCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.GOCode.eventsList0x70e0f0 = function(runtimeScene, context) {
+
+}; //End of gdjs.GOCode.eventsList0x70e0f0
+
+
+gdjs.GOCode.func = function(runtimeScene, context) {
+context.startNewFrame();
+
+gdjs.GOCode.eventsList0x70e0f0(runtimeScene, context);return;
+}
+gdjs['GOCode']= gdjs.GOCode;
